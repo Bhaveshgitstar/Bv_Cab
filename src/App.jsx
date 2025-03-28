206cf27ef56974dfcbdb7a6386694b456a9c79ab
Bhaveshgitstar <bhasha022003@gmail.com>
2025-03-28 18:22:30 +0530

    new routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
+import Home from "./pages/home/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/homePage/HomePage";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 function App() {
   return (
     <>
       <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
+        <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
       </Routes>
